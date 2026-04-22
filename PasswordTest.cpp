b12cc00930588f6182c8343a0ffcb47f8e121f55
Skye Santilla <sesantilla@csuchico.edu>
2026-04-22 11:40:49 -0700

    passed the tests + gcov

--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -7,45 +7,57 @@
 
 class PasswordTest : public ::testing::Test
 {
-	protected:
-		PasswordTest(){} //constructor runs before each test
-		virtual ~PasswordTest(){} //destructor cleans up after tests
-		virtual void SetUp(){} //sets up before each test (after constructor)
-		virtual void TearDown(){} //clean up after each test, (before destructor)
+protected:
+    PasswordTest() {}
+    virtual ~PasswordTest() {}
+    virtual void SetUp() {}
+    virtual void TearDown() {}
 };
 
 TEST(PasswordTest, single_letter_password)
 {
-	Password my_password;
-	ASSERT_EQ(1, my_password.count_leading_characters("Z"));
+    Password my_password;
+    ASSERT_EQ(1, my_password.count_leading_characters("Z"));
 }
 
-TEST(PasswordTest, mixed_case_password)
+TEST(PasswordTest, repeated_letters)
 {
-	Password my_password;
-	ASSERT_EQ(2, my_password.count_leading_characters("ZZz"));
+    Password my_password;
+    ASSERT_EQ(2, my_password.count_leading_characters("ZZz"));
 }
 
-TEST(PasswordTest, alternating_case_password)
+TEST(PasswordTest, lower_case_password)
 {
-	Password my_password;
-	ASSERT_EQ(1, my_password.count_leading_characters("zZzZz"));
+    Password my_password;
+    ASSERT_FALSE(my_password.has_mixed_case("zzz"));
 }
 
-TEST(PasswordTest, quotation_case_password)
+TEST(PasswordTest, empty_string)
 {
-	Password my_password;
-	ASSERT_EQ(0, my_password.count_leading_characters(""));
+    Password my_password;
+    ASSERT_EQ(0, my_password.count_leading_characters(""));
 }
 
-TEST(PasswordTest, quotation_case_password)
+TEST(PasswordTest, mixed_case_Aa)
 {
-	Password my_password;
-	ASSERT_EQ(0, my_password.count_leading_characters("Z"));
+    Password my_password;
+    ASSERT_TRUE(my_password.has_mixed_case("Aa"));
 }
 
-TEST(PasswordTest, has_mixed_case_password)
+TEST(PasswordTest, mixed_case_aA)
 {
-	Password my_password;
-	EXPECT_TRUE(my_password.has_mixed_case("ZzZz"));
+    Password my_password;
+    ASSERT_TRUE(my_password.has_mixed_case("aA"));
+}
+
+TEST(PasswordTest, only_upper)
+{
+    Password my_password;
+    ASSERT_FALSE(my_password.has_mixed_case("ZZZ"));
+}
+
+TEST(PasswordTest, no_letters)
+{
+    Password my_password;
+    ASSERT_FALSE(my_password.has_mixed_case("1234!@#"));
 }
